f95c54dde16cc504d2b71a460bdcc7fe80f32bc6
Wesley Willis <haileyw@gmail.com>
2016-01-25 16:39:32 -0800

    made a javascript clock because I'm super cool

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Your code here
+var nIntervId;
+
+function updateTime() {
+      nIntervId = setInterval(clockTime, 1000);
+    }
+function clockTime() {
+  var today = new Date();
+  var hour = today.getHours().toString();
+  var min = today.getMinutes().toString();
+  var second = today.getSeconds().toString();
+
+  var clock = document.getElementById("clock");
+
+  clock.innerHTML =  hour + ":" + min + ":" + second;
+
+}
